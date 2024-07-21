37174d06e3f43dc4a3b038db8d2ec0438da0b9d0
Dangmir <81549964+Dangmir@users.noreply.github.com>
2024-07-21 21:41:19 +0300

    no large

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,10 @@ export default defineNuxtConfig({
   dayjs: {
     locales: ['ru'],
   },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no"',
+    }
+  }
 })
