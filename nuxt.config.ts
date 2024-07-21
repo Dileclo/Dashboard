1f8e9409125dce1fd6512c76d89e85782f2656a6
Dangmir <81549964+Dangmir@users.noreply.github.com>
2024-07-21 21:44:22 +0300

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no"',
+      viewport: 'width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no',
     }
   }
 })
