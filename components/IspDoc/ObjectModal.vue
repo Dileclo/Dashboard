1d45ebb92456245112615876e28ec124a676980d
Alexey <shargatov99@mail.ru>
2024-07-31 12:44:21 +0300

    1

---
+++ components/IspDoc/ObjectModal.vue
@@ -0,0 +1,58 @@
+<template>
+    <UModal fullscreen @close="modal.close">
+        <UCard :ui="{
+            base: 'h-full flex flex-col',
+            rounded: '',
+            divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+            body: { base: 'grow' }
+        }">
+            <template #header>
+                <div class="flex items-center justify-between">
+                    <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+                        Добавить объект
+                    </h3>
+                    <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+                        @click="modal.close" />
+                </div>
+            </template>
+
+            <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+                <UFormGroup label="Наименование объекта" name="name_object">
+                    <UInput v-model="state.fullname" />
+                </UFormGroup>
+                <UFormGroup label="Застройщик" name="zastroyshik">
+                    <div class="flex gap-4 w-full">
+                        <USelectMenu v-model="state.fullname" class="w-full" searchable />
+                        <UButton label="+" color="gray"/>
+                    </div>
+                </UFormGroup>
+                <UButton type="submit">
+                    Добавить
+                </UButton>
+            </UForm>
+        </UCard>
+    </UModal>
+</template>
+
+<script setup lang="ts">
+import { object, string, date, type InferType } from 'yup';
+import type { FormSubmitEvent } from '#ui/types';
+import { useUserStore } from "~/stores/users"
+
+const usersStore = useUserStore();
+const modal = useModal();
+
+const schema = object({
+    name_object: string().required("Обязательное поле"),
+});
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+    name_object: undefined,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+    usersStore.addUser(event.data)
+    modal.close();
+}
+</script>
--- pages/ispdoc.vue
+++ pages/ispdoc.vue
@@ -1,4 +1,17 @@
 <template>
-    <Navbar label="Исп. документация"></Navbar>
-    
+    <Navbar label="Исп. документация">
+        <template #header>
+            <UButton @click=" modal.open(IspDocObjectModal);" icon="i-heroicons-plus" :ui="{ rounded: 'rounded-full' }"></UButton>
+        </template>
+    </Navbar>
+    <UTable :columns="columns" :rows="objects" />
 </template>
+<script setup type="ts">
+import { IspDocObjectModal } from '#components';
+const modal = useModal()
+const columns = [{
+    key: 'name_object',
+    label: 'Наименование объекта'
+}]
+
+</script>
