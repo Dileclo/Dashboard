f9a295767b3b9c2ac9566251da2c4faf0c3afdfc
Dangmir <81549964+Dangmir@users.noreply.github.com>
2024-08-13 22:48:19 +0300

    13

--- server/api/auth/register.ts
+++ server/api/auth/register.ts
@@ -1,12 +1,27 @@
 import { MongoDBclient, auth } from "~/server/mongo"
+import bcrypt from "bcrypt"
 export default defineEventHandler(async (event) => {
-    const bcrypt = require('bcrypt');
     await MongoDBclient.connect()
     const body = await readBody(event)
-    bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(JSON.parse(body).password, salt, function (err, hash) {
-            console.log(hash)
-        });
-    });
+    
+    const password = JSON.parse(body).password
+
+    const hash = await new Promise((resolve,reject)=>{
+        bcrypt.hash(password,10,(err,hash)=>{
+            if (err) reject(err)
+            resolve(hash)
+        })
+    })
+
+    const isValid = await new Promise((resolve,reject)=>{
+        bcrypt.compare(password,hash,(err,res)=>{
+            if(err) reject(err)
+            resolve(res)
+        })
+    })
+
+
+    console.log(hash, isValid)
+
     await MongoDBclient.close()
 })
