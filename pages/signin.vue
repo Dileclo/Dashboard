287047aa593383fd33e792046291f0a3b59c2ad0
Alexey <shargatov99@mail.ru>
2024-08-13 12:34:44 +0300

    13

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,26 +1,7 @@
 <script lang="ts" setup>
-
+import { object, string, date, number, type InferType } from 'yup';
+import type { FormSubmitEvent } from '#ui/types';
 const { signIn } = useAuth() // uses the default signIn function provided by nuxt-auth
-const formData = reactive({
-    email: '',
-    password: '',
-})
-
-
-const login = async (e) => {
-    try {
-        e.preventDefault()
-        let res = await signIn(
-            { ...formData },
-            { callbackUrl: '/home' } // Where the user will be redirected after a successiful login
-        )
-
-        console.log("res", res);
-
-    } catch (error) {
-        console.log("error", error);
-    }
-}
 
 definePageMeta({
     title: 'Signin',
@@ -32,60 +13,88 @@ definePageMeta({
     },
 })
 
+const items = [{
+    slot: 'account',
+    label: 'Авторизация'
+}, {
+    slot: 'register',
+    label: 'Регистрация'
+}]
+
+const schemaLogin = object({
+    email: string().required("Обязательное поле"),
+    password: string().required("Обязательное поле"),
+});
+const schemaReg = object({
+    email: string().required("Обязательное поле"),
+    password: string().required("Обязательное поле"),
+    username: string().required("Обязательное поле"),
+});
+type SchemaLogin = InferType<typeof schemaLogin>;
+type SchemaReg = InferType<typeof schemaLogin>;
+
+const stateLogin = reactive({
+    email: undefined,
+    password: undefined,
+});
+const stateReg = reactive({
+    email: undefined,
+    password: undefined,
+    username: undefined,
+});
+
+async function onSubmitLogin(event: FormSubmitEvent<SchemaLogin>) {
+    console.log(event.data)
+    signIn('credentials',event.data)
+}
+async function onSubmitRegister(event: FormSubmitEvent<SchemaReg>) {
+    console.log(event.data)
+}
 
 </script>
 
 <template>
-    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12 max-md:p-4">
-        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div
-                class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl max-md:p-4">
-            </div>
-            <div class="relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-20">
-                <div class="max-w-md mx-auto">
-                    <span class="mb-4 justify-center font-medium">Войти</span>
-
-                    <UForm class="space-y-4">
+    <div class="flex flex-col items-center justify-center p-4   h-screen w-full">
+        <UTabs :items="items" class="max-md:w-full md:w-1/3">
+            <template #account="{ item }">
+                <UCard class="">
+                    <template #header>
+                        <div class="font-medium">
+                            Войти
+                        </div>
+                    </template>
+                    <UForm :state="stateLogin" :schema="schemaLogin" class="space-y-4" @submit="onSubmitLogin">
                         <UFormGroup label="E-Mail">
-                            <UInput v-model="formData.email" type="email" placeholder="Введите email" />
+                            <UInput v-model="stateLogin.email" type="email" placeholder="Введите email" />
                         </UFormGroup>
                         <UFormGroup label="Пароль">
-                            <UInput v-model="formData.email" type="email" placeholder="Введите email" />
+                            <UInput v-model="stateLogin.password" type="password" placeholder="Введите пароль" />
                         </UFormGroup>
+                        <UButton label="Войти" type="submit" color="gray" />
                     </UForm>
-                </div>
-
-                <div class="w-full flex justify-center">
-                    <button
-                        class=" mt-4 flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                        <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="-0.5 0 48 48"
-                            version="1.1">
-                            <defs> </defs>
-                            <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <g id="Color-" transform="translate(-401.000000, -860.000000)">
-                                    <g id="Google" transform="translate(401.000000, 860.000000)">
-                                        <path
-                                            d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
-                                            id="Fill-1" fill="#FBBC05"> </path>
-                                        <path
-                                            d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
-                                            id="Fill-2" fill="#EB4335"> </path>
-                                        <path
-                                            d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
-                                            id="Fill-3" fill="#34A853"> </path>
-                                        <path
-                                            d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
-                                            id="Fill-4" fill="#4285F4"> </path>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                        <span>Continue with Google</span>
-                    </button>
-                </div>
-
-            </div>
-        </div>
+                </UCard>
+            </template>
+            <template #register="{ item }">
+                <UCard class="">
+                    <template #header>
+                        <div class="font-medium">
+                            Зарегистрироваться
+                        </div>
+                    </template>
+                    <UForm :state="stateReg" :schema="schemaReg" class="space-y-4" @submit.prevent="onSubmitRegister">
+                        <UFormGroup label="E-Mail">
+                            <UInput v-model="stateReg.email" type="email" placeholder="Введите email" />
+                        </UFormGroup>
+                        <UFormGroup label="Имя пользователя">
+                            <UInput v-model="stateReg.username" type="username" placeholder="Введите имя пользователя" />
+                        </UFormGroup>
+                        <UFormGroup label="Пароль">
+                            <UInput v-model="stateReg.password" type="password" placeholder="Введите пароль" />
+                        </UFormGroup>
+                        <UButton type="submit" label="Зарегистрироваться" color="gray" />
+                    </UForm>
+                </UCard>
+            </template>
+        </UTabs>
     </div>
 </template>
