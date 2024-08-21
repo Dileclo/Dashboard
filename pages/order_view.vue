5494590707265e503fd1c9afee879974240aa31c
Alexey <shargatov99@mail.ru>
2024-08-21 16:58:31 +0300

    213

--- pages/order_view.vue
+++ pages/order_view.vue
@@ -2,9 +2,16 @@
     <UContainer>
         <div class="grid grid-cols-2 p-4 gap-4">
             <div v-for="item in orderStore.orders">
-                <UCard>{{ item }}</UCard>
+                <UCard>
+                    <template #header>
+                        <div>
+                            {{ item.order_id }}
+                        </div>
+                    </template>
+                    {{ item.customer }}
+                        <UTable :rows="item.bucket"/>
+                </UCard>
             </div>
-            <div>PIDAR</div>
         </div>
     </UContainer>
 </template>
