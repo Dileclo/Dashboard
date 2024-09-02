b775b1f795b079458f57f8704d2f81972aae5c90
Alexey Shargatov <shargatov99@mail.ru>
2024-09-02 06:40:38 +0300

    123

--- server/api/order/add.ts
+++ server/api/order/add.ts
@@ -1,20 +1,29 @@
-import { MongoDBclient, order, material } from "~/server/mongo"
-import { getToken } from '#auth'
+import { MongoDBclient, order, material } from "~/server/mongo";
+import { getToken } from "#auth";
 
 export default defineEventHandler(async (event) => {
-    const token = await getToken({ event })
-    await MongoDBclient.connect()
-    const body = await readBody(event)
-    const body_parse = JSON.parse(body)
-    const r = async () => {
-        body_parse.bucket.forEach(element => {
-            if (element.type == "Металлопрокат") {
-                const total_length = Number(element.count) * (Number(element.length) / 1000)
-            }
-            const res = material.find({ 'auth': token?.email, 'thickness': element.thickness, 'color': element.color }).toArray()
-            console.log(res)
-        })
+  const token = await getToken({ event });
+  await MongoDBclient.connect();
+  const body = await readBody(event);
+  const body_parse = JSON.parse(body);
+  body_parse.bucket.forEach((element) => {
+    console.log(element);
+    if (element.type == "Металлопрокат") {
+      const total_length =
+        Number(element.count) * (Number(element.length) / 1000);
+      console.log(total_length);
+
+      const res = material.updateOne(
+        {
+          auth: token?.email,
+          thickness: element.thickness,
+          color: element.color,
+        },
+        { $inc: { length: -total_length } },
+        { upsert: true }
+      );
     }
-    await order.insertOne({ ...body_parse, auth: token?.email })
-    await MongoDBclient.close()
-})
+  });
+  await order.insertOne({ ...body_parse, auth: token?.email });
+  await MongoDBclient.close();
+});
